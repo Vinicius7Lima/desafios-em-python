526a3c8b23139d10cd9e02e41c79d0dc0ee4ac3d
Vinicius7Lima <vynnhydossantos@gmail.com>
2022-02-09 14:01:39 -0300

    priemira alteração

--- variaveis.py
+++ variaveis.py
@@ -7,16 +7,25 @@ idade = 40
 altura = 1.60
 peso = 65
 imc = peso / (altura ** 2)
+
 if idade > 18:
     e_maior = 'Sim'
 else:
     e_maior = 'Não'
 
+dados = ['nome', 'idade', 'altura', 'peso', 'imc', 'Maior de Idade']
+tamanho = []
+
+for quantidade in enumerate(dados):
+    count = 30 - len(quantidade)
+    tamanho.append(count)
+
+
 print(f"""
-Nome: {nome:>14}
-Idede: {idade:>11} Anos
-altura: {altura:>12.2f}m
-peso: {peso:>15.2f}Kg
-IMC: {imc:>16.2f}
-Maior de Idade? {e_maior}
+{'Nome:':<{tamanho[0]}} {nome}
+{'Idede:':<{tamanho[1]}} {idade} Anos
+{'altura:':<{tamanho[2]}} {altura:.2f}m
+{'peso:':<{tamanho[3]}} {peso:.2f}Kg
+{'IMC:':<{tamanho[4]}} {imc:.2f}
+{'Maior de Idade:':<{tamanho[5]}} {e_maior}
     """)
